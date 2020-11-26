546977a6a388281c82ba43ce5209bb9fe3567453
Kishor Naik <kishor.naik011.net@outlook.com>
2020-11-26 20:24:12 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Sol_Demo/Styles/ButtonStyle.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Sol_Demo.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonStyle : ResourceDictionary // Remove Content Page Class and Add Resource Dictionary class
+    {
+        public ButtonStyle()
+        {
+            InitializeComponent();
+        }
+    }
+}
